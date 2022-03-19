05cd8e9edbcf922b9ada0824c089a12b1ed1dca0
Bruno Grajales <82546939+bgrajales@users.noreply.github.com>
2022-03-19 16:09:17 -0300

    Update

--- src/controllers/movies/markMovieAsWatched.js
+++ src/controllers/movies/markMovieAsWatched.js
@@ -6,26 +6,28 @@ module.exports = (request, response) => {
     const movie = request.body
 
     console.log(user, movie)
-    // userModel.findOne({
-    //     userName: user.userName
-    // }).then(user => {
+    userModel.findOne({
+        userName: user.userName
+    }).then(user => {
 
-    //     user.movies.push({
-    //         id: movie.id,
-    //         posterPath: movie.posterPath,
-    //         runTime: movie.runTime
-    //     })
+        user.movies.push({
+            id: movie.id,
+            posterPath: movie.posterPath,
+            runTime: movie.runTime
+        })
 
-    //     markModified(user, 'movies')
+        markModified(user, 'movies')
 
-    //     user.save().then(() => {
-    //         response.status(200).end()
-    //     }).catch(error => {
-    //         response.status(500).end()
-    //     })
+        user.save().then(() => {
+            response.status(200).end()
+        }).catch(error => {
+            response.status(500).end()
+        })
 
-    // }).catch(error => {
-    //     response.status(500).end()
-    // })
+    }).catch(error => {
+        response.status(500).end()
+    })
+
+
 
 }
